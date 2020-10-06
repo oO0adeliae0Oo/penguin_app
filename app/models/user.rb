beccc707dd89ba8c40833e381040734d375ac41f
adeliae <oo0adeliae0oo@adeliaenoMacBook-Pro.local>
2020-10-06 16:32:54 +0900

    valid mail address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence:true, length: {maximum: 20}
-    validates :email, presence:true, length: {maximum: 250}
+    Valid_email = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence:true, length: {maximum: 250},
+                format: { with: Valid_email }
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Penguin User", email: "user@example.com")
+    @user = User.new(name: "Penguin User", email: "user@penguin.com")
   end
 
   test "valid" do
@@ -27,7 +27,12 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a"*200 + "@example.com"
   end
 
-  # test "valid email address" do
-  #   valid_email_addresses = 
-  # end
+  test "valid email address" do
+    valid_email_address = %w[user@penguin.com USER@PenGuin.COM U-Se_R@penguin.adeliae.com]
+
+    valid_email_address.each do |valid_email_address|
+      @user.email = valid_email_address
+      assert @user.valid?, "#{valid_email_address.inspect} should be valid"
+    end
+  end
 end
