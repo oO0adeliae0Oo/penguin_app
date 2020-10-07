630394984cfe8f118da42fcde8211e0b6fe3d31e
adeliae <oo0adeliae0oo@adeliaenoMacBook-Pro.local>
2020-10-07 14:28:52 +0900

    signup周り調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root'static_pages#home'
 
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get '/signup', to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
