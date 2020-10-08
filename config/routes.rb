eb793cf9093be20c88eb0bab455eddff7c269f68
adeliae <oo0adeliae0oo@adeliaenoMacBook-Pro.local>
2020-10-08 15:05:34 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'session/new'
+
   root'static_pages#home'
 
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
 
   get '/signup', to: 'users#new'
   resources :users
 
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  
+  post '/login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
