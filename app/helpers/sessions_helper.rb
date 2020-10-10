15e9ab1ca458793041dccab46175df7fc6c2d213
adeliae <oo0adeliae0oo@adeliaenoMacBook-Pro.local>
2020-10-10 19:09:30 +0900

    update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,10 @@ module SessionsHelper
             end
         end
     end
+
+    def current_user?(user)
+        user == current_user
+    end
     
     def logged_in?
         !current_user.nil?
@@ -38,5 +42,14 @@ module SessionsHelper
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    def redirect_back(default)
+        redirect_to (session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
